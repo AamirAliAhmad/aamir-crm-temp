ed869dcad58fe24aa0bb1b3f20e6e7a52bf6d9e5
AamirAliAhmad <mohdaamir.amr@gmail.com>
2022-02-01 13:01:45 +0530

    tesing file commit

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -5,6 +5,7 @@ export default function HomePage()
     return(
         <div>
             {/* <MasterLayout></MasterLayout> */}
+            {/* aamir test change */}
             <ReactNavSideMenu></ReactNavSideMenu>
         </div>
     );
